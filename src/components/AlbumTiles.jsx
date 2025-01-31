73f12d81f366686b55ea41d2ad185c298fbd3173
Ritik <ritikkashyap7404@gmail.com>
2025-02-01 00:16:52 +0530

    fixed bugs

--- src/components/AlbumTiles.jsx
+++ src/components/AlbumTiles.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import MusicPlaceholder from "../assets/images/Musicplaceholder"
 
 function AlbumTiles({ album }) {
     console.log(album.type)
     if (album.type != "Single") {
         return (
             <div className='flex flex-col gap-3 items-center justify-center w-fit h-fit'>
-                <img className='h-40 rounded-full' src={album.thumbnailUrl ? album.thumbnailUrl : MusicPlaceholder} alt={album.artist} />
+                <img className='h-40 rounded-full' src={album.thumbnailUrl ? album.thumbnailUrl : ""} alt={album.artist} />
                 <div className='flex flex-col items-center'>
                     <p className='line-clamp-1 max-w-[20ch] text-white'>{album.title}</p>
                     <p className='line-clamp-1 max-w-[40ch]'>{album.artist}</p>
