cb4f4558688370abf97dbd65c9f8cc542ffabe05
Ritik <ritikkashyap7404@gmail.com>
2025-02-03 21:46:45 +0530

    PWA

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
 
 
     return (
-        <div className="navbar bg-[#001919] p-5  fixed top-0 z-10 shadow-lg">
+        <div className="navbar bg-[#001919] p-5 pr-3  fixed top-0 z-10 shadow-lg">
             <div className="flex-1">
                 <Link className="text-xl font-semibold text-white flex items-center gap-2" to={"/"}><img className='w-12' src='/images/music-192x192.png' alt="music"/><p className='hidden lg:block'>Music</p></Link>
             </div>
